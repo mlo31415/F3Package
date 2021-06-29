a38028c4abb60344790a892fcb4dff4df6bbfc68
mlo31415 <mlo@baskerville.org>
2021-06-29 15:28:39 -0400

    Correct comments

--- F3Reference.py
+++ F3Reference.py
@@ -7,11 +7,10 @@ from HelpersPackage import WindowsFilenameToWikiPagename
 @dataclass(order=False)
 class F3Reference:
     #def __init__(self, LinkWikiName: Optional[str]=None, LinkDisplayText: Optional[str]=None, ParentPageName: Optional[str]=None, FanacURL: Optional[str]=None) -> None:
-    LinkWikiName: str=""         # The wiki name of the page being linked to if it is a link to Fancy 3 (else None)
-                                            # Note that this not the wiki page's canonical name.
-    LinkDisplayText: str=""   # The text actually used on the page for the link [[the stuff between the brackets]]
-    ParentPageName: str=""     # If from a reference to Fancy, the name of the Fancy page it is on (else None)
-    FanacURL: str=""                 # If a reference to fanac.org, the URL of the page it was on (else None)
+    LinkWikiName: str=""        # The name of the wiki page linked to.  For simple links it is the same as LinkDisplayText
+    LinkDisplayText: str=""     # The text displayed for the link on the wiki page.  For simple links it is the same as LinkWikiName
+    ParentPageName: str=""      # If from a reference to Fancy, the name of the Fancy page it is on (else None)
+    FanacURL: str=""            # If a reference to fanac.org, the URL of the page it was on (else None)
 
     def Copy(self, val: F3Reference):
         if type(val) is F3Reference:
